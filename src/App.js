abc682e17100cac5e65e3276488e9b7bafc15ec3
Anton Koval <toniokovalski@gmail.com>
2023-09-19 19:23:48 +0200

    add base route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/SpaceX-launches-test">
       <Routes>
         <Route exact path="/" element={<LaunchTable />} />
         <Route path="/launch/:id" element={<LaunchDetails />} />
